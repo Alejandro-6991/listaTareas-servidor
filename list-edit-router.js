6c693f279a7f349b9cc7904b7e91ac0b86c4950f
Alejandro-12 <h.alejandro1021@gmail.com>
2023-09-23 14:56:31 -0600

    lista router

--- list-edit-router.js
+++ list-edit-router.js
@@ -1,53 +1,67 @@
 const express = require('express');
-
 const router = express.Router();
 
+const tarea = [
+  {
+    "id": 1,
+    "description": "dormir",
+    "isCompleted": false
+  },
+  {
+    "id": 2,
+    "description": "hacer tramites bancarios",
+    "isCompleted": true
+  },
+  {
+    "id": 3,
+    "description": "ir al supermercado",
+    "isCompleted": false
+  }
+];
 
-router.get('/', (req,res) => {
-  res.send('ruta raiz (get)')
-} )
+router.get('/', (req, res) => {
+  res.json(tarea);
+});
 
-router.get('/:id', (req,res) => {
-  const id = req.params.id;
-  res.send('ruta para obtener un list:'+id)
-} )
+router.get('/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const tareaEncontrada = tarea.find(item => item.id === id);
+  if (tareaEncontrada) {
+    res.json(tareaEncontrada);
+  } else {
+    res.status(404).json({ message: 'Tarea no encontrada' });
+  }
+});
 
-
-// crear tarea
-router.post('/:id', (req, res) => {
-  const nuevaTarea = req.body; 
-  tareas.push(nuevaTarea);
+router.post('/', (req, res) => {
+  const nuevaTarea = req.body;
+  tarea.push(nuevaTarea);
   res.status(201).json(nuevaTarea);
 });
 
-// eleminar tarea
 router.delete('/:id', (req, res) => {
   const tareaId = parseInt(req.params.id);
-  const tareaIndex = tareas.findIndex(tarea => tarea.id === tareaId);
+  const tareaIndex = tarea.findIndex(item => item.id === tareaId);
 
   if (tareaIndex !== -1) {
-    const tareaEliminada = tareas.splice(tareaIndex, 1);
+    const tareaEliminada = tarea.splice(tareaIndex, 1);
     res.json(tareaEliminada[0]);
   } else {
     res.status(404).json({ message: 'Tarea no encontrada' });
   }
 });
 
-// actualizar tarea
-router.put('/tareas/:id', (req, res) => {
+router.put('/:id', (req, res) => {
   const tareaId = parseInt(req.params.id);
-  const tareaActualizada = req.body; 
+  const tareaActualizada = req.body;
 
-  const tareaIndex = tareas.findIndex(tarea => tarea.id === tareaId);
+  const tareaIndex = tarea.findIndex(item => item.id === tareaId);
 
   if (tareaIndex !== -1) {
-    tareas[tareaIndex] = { ...tareas[tareaIndex], ...tareaActualizada };
-    res.json(tareas[tareaIndex]);
+    tarea[tareaIndex] = { ...tarea[tareaIndex], ...tareaActualizada };
+    res.json(tarea[tareaIndex]);
   } else {
     res.status(404).json({ message: 'Tarea no encontrada' });
   }
 });
-
-
-
 module.exports = router;
